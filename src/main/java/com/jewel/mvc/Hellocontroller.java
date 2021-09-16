15391dd946a612bd40160ab1cb4aecd77d0d201d
CrisCheng <1349668699@qq.com>
2021-09-16 18:28:55 +0800

    创建请求控制器

---
+++ src/main/java/com/jewel/mvc/Hellocontroller.java
@@ -0,0 +1,13 @@
+package com.jewel.mvc;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 控制器
+ *
+ * @author chenggong
+ * @date 2021/09/16
+ */
+@Controller
+public class Hellocontroller {
+}
