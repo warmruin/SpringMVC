82cc1a939e1338d8d6ec59577f996371389f11b6
Gong.Cheng <1349668699@qq.com>
2021-10-31 21:04:08 +0800

    restful 风格的实现

---
+++ src/main/java/com/jewel/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.jewel.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    /**
+     * 使用restful模拟用户资源的增删改查
+     *  user get  查询所有用户信息
+     */
+
+
+}
