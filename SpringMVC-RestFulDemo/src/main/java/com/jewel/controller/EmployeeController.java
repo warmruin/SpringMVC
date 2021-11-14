16c2b32a6b217266892bfec8068f25855d355707
Gong.Cheng <1349668699@qq.com>
2021-11-14 14:22:42 +0800

    SpringMVC restful 实现列表功能

--- SpringMVC-RestFulDemo/src/main/java/com/jewel/controller/EmployeeController.java
+++ SpringMVC-RestFulDemo/src/main/java/com/jewel/controller/EmployeeController.java
@@ -1,14 +1,30 @@
 package com.jewel.controller;
 
+import com.jewel.bean.Employee;
 import com.jewel.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
+
 @Controller
 public class EmployeeController {
 
 
     @Autowired
     private EmployeeDao employeeDao;
+
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getEmployeeList(Model model){
+        Collection<Employee> employeeList = employeeDao.getAll();
+        model.addAttribute("employeeList", employeeList);
+        return "employee_list";
+    }
 
 
 }
